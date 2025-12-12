eadc3d462df2b098d818104a45b7dfbf2b46a7b2
AmirReza Alami <amirreza.flutter@gmail.com>
2025-12-12 15:19:40 +0330

    Fix bottom navigation bar pixel overflow

--- lib/views/widgets/bottom_navigation_bar.dart
+++ lib/views/widgets/bottom_navigation_bar.dart
@@ -1,4 +1,5 @@
 import 'package:coffee_app_ui/controllers/bottom_navbar_controller.dart';
+import 'package:coffee_app_ui/core/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -9,58 +10,61 @@ class BottomNavBar extends StatelessWidget {
   final BottomNavbarController _controller = Get.put(BottomNavbarController());
   @override
   Widget build(BuildContext context) {
-    return Obx(() => SizedBox(
-          height: 65,
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            backgroundColor: Colors.white,
-            fixedColor: Colors.green[800],
-            unselectedItemColor: Colors.grey,
-            selectedLabelStyle: TextStyle(
-              fontFamily: 'OpenSansBold',
-              fontSize: ScreenUtil().setSp(12),
-              fontWeight: FontWeight.bold,
+    return SafeArea(
+      child: Obx(() => SizedBox(
+            height: 65,
+            child: BottomNavigationBar(
+              elevation: 0,
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Palette.white,
+              fixedColor: Colors.green[700],
+              unselectedItemColor: Colors.grey,
+              selectedLabelStyle: TextStyle(
+                fontFamily: 'OpenSansBold',
+                fontSize: ScreenUtil().setSp(12),
+                fontWeight: FontWeight.bold,
+              ),
+              unselectedLabelStyle: TextStyle(
+                fontFamily: 'OpenSansBold',
+                fontSize: ScreenUtil().setSp(12),
+                fontWeight: FontWeight.bold,
+              ),
+              showUnselectedLabels: true,
+              showSelectedLabels: true,
+              items: [
+                BottomNavigationBarItem(
+                  label: 'Home',
+                  icon: Icon(
+                    Icons.home,
+                    size: ScreenUtil().setSp(25),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  label: 'Favorite',
+                  icon: Icon(
+                    Icons.favorite_outline,
+                    size: ScreenUtil().setSp(25),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  label: 'Cart',
+                  icon: Icon(
+                    Icons.shopping_cart_outlined,
+                    size: ScreenUtil().setSp(25),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  label: 'Person',
+                  icon: Icon(
+                    Icons.person_outline_rounded,
+                    size: ScreenUtil().setSp(25),
+                  ),
+                ),
+              ],
+              currentIndex: _controller.currentIndex,
+              onTap: _controller.selectItem,
             ),
-            unselectedLabelStyle: TextStyle(
-              fontFamily: 'OpenSansBold',
-              fontSize: ScreenUtil().setSp(12),
-              fontWeight: FontWeight.bold,
-            ),
-            showUnselectedLabels: true,
-            showSelectedLabels: true,
-            items: [
-              BottomNavigationBarItem(
-                label: 'Home',
-                icon: Icon(
-                  Icons.home,
-                  size: ScreenUtil().setSp(24),
-                ),
-              ),
-              BottomNavigationBarItem(
-                label: 'Favorite',
-                icon: Icon(
-                  Icons.favorite_outline,
-                  size: ScreenUtil().setSp(24),
-                ),
-              ),
-              BottomNavigationBarItem(
-                label: 'Cart',
-                icon: Icon(
-                  Icons.shopping_cart_outlined,
-                  size: ScreenUtil().setSp(24),
-                ),
-              ),
-              BottomNavigationBarItem(
-                label: 'Person',
-                icon: Icon(
-                  Icons.person_outlined,
-                  size: ScreenUtil().setSp(22),
-                ),
-              ),
-            ],
-            currentIndex: _controller.currentIndex,
-            onTap: _controller.selectItem,
-          ),
-        ));
+          )),
+    );
   }
 }
